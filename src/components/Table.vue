6f2a6ec2e877416bc160d7ff01dbea3e287fb279
Ihwan Edi Saputro <ihwan1999@gmail.com>
2021-01-21 23:41:31 +0700

    Edit the table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,22 +1,24 @@
 <template>
-  <v-card>
-    <v-card-title>
-      Nutrition
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :search="search"
-    ></v-data-table>
-  </v-card>
+  <v-container>
+    <v-card>
+      <v-card-title>
+        Skills
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="skills"
+        :search="search"
+      ></v-data-table>
+    </v-card>
+  </v-container>
 </template>
 <script>
 export default {
@@ -24,98 +26,60 @@ export default {
     return {
       search: "",
       headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
+        { text: "Name", value: "name" },
+        { text: "Proficiency (1-10)", value: "proficiency" },
+        { text: "Field", value: "field" },
       ],
-      desserts: [
+      skills: [
         {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%",
+          name: "Django",
+          proficiency: 7.5,
+          field: "Backend Development",
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%",
+          name: "Figma",
+          proficiency: 7.5,
+          field: "UI/UX",
         },
         {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%",
+          name: "Java",
+          proficiency: 6.5,
+          field: "Software Development",
         },
         {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%",
+          name: "Agile/Scrum",
+          proficiency: 7.5,
+          field: "Software Development"
         },
         {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%",
+          name: "English",
+          proficiency: 7.5,
+          field: "Language"
         },
         {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%",
+          name: "Vue.js",
+          proficiency: 6,
+          field: "Frontend Development"
         },
         {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%",
+          name: "User Research",
+          proficiency: 7,
+          field: "UI/UX"
         },
         {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%",
+          name: "Adobe Illustrator",
+          proficiency: 6.5,
+          field: "Design"
         },
         {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%",
+          name: "Adobe Lightroom",
+          proficiency: 6.5,
+          field: "Design"
         },
         {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%",
+          name: "Git",
+          proficiency: 7,
+          field: "Software Development"
         },
       ],
     };
